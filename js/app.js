9fe7971bb623f7452b7a7502f683bbb2e5b82f75
uchaa03 <uchasousaa@gmail.com>
2024-10-24 22:50:53 +0200

    V2 - Implementado muestreo y localstorage

--- js/app.js
+++ js/app.js
@@ -2,20 +2,27 @@
 //Selectores
 const textarea = document.querySelector("#tweet")
 const submit = textarea.nextElementSibling
-const title = document.querySelector("h2")
 const container = document.querySelector(".container")
+const divHead = container.lastElementChild.lastElementChild
 
 //Listeners
 submit.addEventListener("click", checkTweet)
+//Para darle consistencia en navegador
+document.addEventListener("DOMContentLoaded", () => {
+    tweets = JSON.parse(localStorage.getItem("tweets")) || [] //Si esta vacio lo deja vacio
+    showTweets()
+})
+
 
 //Variables a usar
-const tweets = []
+let tweets = []
 const divError = document.createElement("div")
 divError.classList.add("error")
 container.appendChild(divError)
 divError.textContent = "ERROR, INTRODUCE UN TWEET"
 divError.style.visibility = 'hidden'
-
+const divContainer = document.createElement("div")
+divHead.appendChild(divContainer)
 
 //Funcíon para checkear lo que se sube
 function checkTweet(e) {
@@ -34,5 +41,33 @@ function checkTweet(e) {
 }
 
 function showTweets() {
-    console.log(tweets)
+    divContainer.innerHTML = ""
+    //Los vamos agregando debajo del titulo
+    tweets.forEach(tweet => {
+        //Creamos un parrado donde ir agregando los tweets
+        const p = document.createElement("p")
+        //Creamos un boton de eliminación de tweets
+        const remove = document.createElement("a")
+        remove.textContent = "x"
+        remove.classList.add("borrar-tweet")
+        remove.onclick = removeTweet
+        p.textContent = tweet
+        p.appendChild(remove)
+        divContainer.appendChild(p)
+    })
+    updateLocalStorage()
+}
+
+function removeTweet(e) {
+    const pSelector = e.target.parentElement
+    const removeTweet = pSelector.firstChild.textContent
+    tweets = tweets.filter(tweet => tweet !== removeTweet)
+    updateLocalStorage()
+    showTweets()
+}
+
+//Función para actualizar el localStorage
+function updateLocalStorage() {
+    const cartProductString = JSON.stringify(tweets)
+    localStorage.setItem("tweets", cartProductString)
 }
